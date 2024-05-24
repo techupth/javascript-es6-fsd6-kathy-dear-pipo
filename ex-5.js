f43f5d7d2516fa965ba67607b212754ddafa94da
KathyKZ <kathykate.sooksutum@gmail.com>
2024-05-24 23:42:43 +0700

    feat:ES6 homework

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,13 @@
-function sum() {
+function sum(...input) {
   // Start coding here !
+
+let arrayInput = [...input];
+let total = 0;
+for (let index = 0; index < arrayInput.length; index++) {
+    total += arrayInput[index];
+    
+}
+return total;
 }
 
 let result1 = sum(1, 2, 3, 4, 5, 6, 7, 8);
--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,5 @@
-let height = undefined;
-let result;
+let height = null;
+let result = height ?? 
+
 
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
